f63b88e2d8f78b01e4d195b7147e91e623832332
rye <rvyeinq@gmail.com>
2023-02-27 08:48:15 -0600

    Update main.c

--- main.c
+++ main.c
@@ -8,31 +8,35 @@ int main(void) {
   char adjective[80]; 
   int play;  
   
-  // stdin & stdout  
+  // Menu, choose between play, or quit
   printf("--------\nMADLIBS!\n--------\n");  
   printf("Enter a Number to continue.\n\n"); 
   printf("\n[1] Play\n"); printf("[2] Quit\n\n");  
   
   scanf("%i", &play);  
+  
+  // If the user chooses play (1)
   if (play == 1) { 
+    
+    // Asking for variables, for the madlib
     printf("\nName a food: ");
     scanf("%s", food);
-
     printf("\nGive me a name: ");
     scanf("%s", name);
-    
     printf("\nName an adjective: ");
     scanf("%s", adjective);
-
+    
+    // Prints madlib to stdout
     printf("\n\nHere's your MADLIB!\n");
-
-    // cited from https://kidscodecs.com/python-mad-libs/#:~:text=AN%20EXAMPLE%20MADLIB&text=It%20was%20pizza%20day%20at,and%20ran%20through%20the%20playground
     printf("It was %s day at school, and %s was super %s for lunch.", food, name, adjective);
 
   } 
+  // if the user chooses, quit (2)
   if (play == 2) { 
     printf("\nGoodbye!");
   } 
+  
+  // error catching
   else { 
     printf("\nPlease enter a number, quitting.\n");
   }
